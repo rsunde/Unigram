f1064f0fba96be94e5d811778146f7249e8cf1c5
Fela Ameghino <frayx@live.it>
2017-06-08 23:41:03 +0200

    Flatting down

--- Unigram/Unigram/Controls/BubbleListView.cs
+++ Unigram/Unigram/Controls/BubbleListView.cs
@@ -138,73 +138,7 @@ namespace Unigram.Controls
                 }
                 else
                 {
-                    var message = item as TLMessage;
-                    if (message != null && message.ToId is TLPeerChat || message.ToId is TLPeerChannel && !message.IsPost)
-                    {
-                        //// WARNING: this is an hack. 
-                        //// We should verify that this still works every Windows release!
-
-                        //// ItemsStackPanel has a bug and it disregards
-                        //// about headers width when measuring the items,
-                        //// causing them to take all the ItemsStackPanel width
-                        //// and getting truncated out of it at the right side.
-
-                        //// The code below prevents this behavior adding
-                        //// the exact padding/margin to compehensate the headers width
-                        //if (message.IsOut)
-                        //{
-                        //    bubble.Padding = new Thickness(44, 0, 12, 0);
-                        //    bubble.Margin = new Thickness(12, 0, 0, 0);
-                        //    bubble.HorizontalAlignment = HorizontalAlignment.Right;
-                        //}
-                        //else
-                        //{
-                        //    bubble.Padding = new Thickness(12, 0, 56, 0);
-                        //    bubble.Margin = new Thickness(0, 0, 44, 0);
-                        //    bubble.HorizontalAlignment = HorizontalAlignment.Left;
-                        //}
-
-                        if (message.IsOut)
-                        {
-                            if (message.IsSticker())
-                            {
-                                bubble.Padding = new Thickness(12, 0, 12, 0);
-                            }
-                            else
-                            {
-                                bubble.Padding = new Thickness(52, 0, 12, 0);
-                            }
-                        }
-                        else
-                        {
-                            if (message.IsSticker())
-                            {
-                                bubble.Padding = new Thickness(52, 0, 12, 0);
-                            }
-                            else
-                            {
-                                bubble.Padding = new Thickness(52, 0, 52, 0);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (message.IsSticker())
-                        {
-                            bubble.Padding = new Thickness(12, 0, 12, 0);
-                        }
-                        else
-                        {
-                            if (message.IsOut && !message.IsPost)
-                            {
-                                bubble.Padding = new Thickness(52, 0, 12, 0);
-                            }
-                            else
-                            {
-                                bubble.Padding = new Thickness(12, 0, 52, 0);
-                            }
-                        }
-                    }
+                    bubble.Padding = new Thickness(52, 0, 12, 0);
                 }
             }
 
